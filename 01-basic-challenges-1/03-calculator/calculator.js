8122f1f532d5e58418c8e8982b0d95617a9bafd0
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-11-22 12:54:47 +0530

    calculator

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,17 @@
-function calculator() {}
+function calculator(n1, n2, op) {
+  if (op === "+") {
+    return (result = n1 + n2);
+  } else if (op === "-") {
+    return (result = n1 - n2);
+  } else if (op === "*") {
+    return (result = n1 * n2);
+  } else if (op === "/") {
+    return (result = n1 / n2);
+  } else {
+    return "invalid operator";
+  }
+}
+
+calculator(2, 2, "+");
 
 module.exports = calculator;
