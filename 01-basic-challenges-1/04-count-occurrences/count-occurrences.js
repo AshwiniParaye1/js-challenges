456739c823a4bcb67e6ac7851ee9d26e10069f77
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-11-22 14:30:57 +0530

    count-occurence solved

--- 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ 01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,20 @@
-function countOccurrences() {}
+function countOccurrences(str, char) {
+  //   let noOfOccur = 0;
+  //   for (let i = 0; i < str.length; i++) {
+  //     if (str[i] === char) {
+  //       noOfOccur++;
+  //     }
+  //   }
+  //   console.log(noOfOccur);
+  //   return noOfOccur;
+
+  let noOfOccurence = str.split(char).length - 1;
+
+  console.log(noOfOccurence);
+
+  return noOfOccurence;
+}
+
+countOccurrences("ssfksAAAjdfoihKAJHKjswe", "s");
 
 module.exports = countOccurrences;
