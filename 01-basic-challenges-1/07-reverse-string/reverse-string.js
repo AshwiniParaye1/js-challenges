8c91ce25088677a570ebf29c20d7692de57c5f6e
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-11-22 15:58:58 +0530

    reverse string

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,23 @@
-function reverseString() {}
+// Write a function called `reverseString` that takes in a string and returns the reverse of that string. In this section, we are really focusing on loops without using any built-in methods, so try that first. If you get stuck, you can always use the built-in methods to solve the problem.
+
+function reverseString(str) {
+  //   const reverse = str.split("").reverse().join("");
+
+  //   console.log(reverse);
+
+  //   return reverse;
+
+  let reverse = "";
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reverse += str[i];
+  }
+
+  console.log(reverse);
+
+  return reverse;
+}
+
+reverseString("space node");
 
 module.exports = reverseString;
