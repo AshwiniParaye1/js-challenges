49750a182f0d73ba4e8d67ad625bdfd69b441762
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-11-22 14:47:46 +0530

    find max num

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,23 @@
-function findMaxNumber() {}
+function findMaxNumber(arr) {
+  //   let largest = Math.max(...arr);
+
+  //   console.log(largest);
+
+  //   return largest;
+
+  let largestNum = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > largestNum) {
+      largestNum = arr[i];
+    }
+  }
+
+  console.log(largestNum);
+
+  return largestNum;
+}
+
+findMaxNumber([1, 34, 8, 2, 3]);
 
 module.exports = findMaxNumber;
