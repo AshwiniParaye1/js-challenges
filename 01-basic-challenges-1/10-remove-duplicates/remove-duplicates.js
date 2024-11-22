483a87dd6d7f4df7a9311adfe142438d28a88334
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-11-22 16:27:03 +0530

    duplicates

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,21 @@
-function removeDuplicates() {}
+//Write a function called `removeDuplicates` that takes in an array and returns a new array with duplicates removed.
+
+//The array can contain any data type
+
+function removeDuplicates(arr) {
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!newArr.includes(arr[i])) {
+      newArr.push(arr[i]);
+    }
+  }
+
+  console.log(newArr);
+
+  return newArr;
+}
+
+removeDuplicates([1, 1, 12, 1, 1, 1, 14, 1, 1, 1]);
 
 module.exports = removeDuplicates;
