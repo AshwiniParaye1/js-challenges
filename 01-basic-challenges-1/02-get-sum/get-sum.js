96e0f72f45644a18ad51bc8f5dd0a9c3221e316d
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-11-22 12:49:04 +0530

    02-getSum

--- 01-basic-challenges-1/02-get-sum/get-sum.js
+++ 01-basic-challenges-1/02-get-sum/get-sum.js
@@ -1,3 +1,12 @@
-function getSum() {}
+function getSum(n1, n2) {
+  const num1 = n1;
+  const num2 = n2;
+
+  const result = num1 + num2;
+
+  return result;
+}
+
+getSum(2, 4);
 
 module.exports = getSum;
