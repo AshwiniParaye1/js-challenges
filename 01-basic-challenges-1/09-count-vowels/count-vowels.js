0f90206270734b0492790cbf81790099386bcea1
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-11-22 16:21:56 +0530

    count vowels

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,28 @@
-function countVowels() {}
+// Write a function called `countVowels` that takes in a string and returns the number of vowels in the string.
+
+function countVowels(str) {
+  let count = 0;
+  const formattedStr = str.toLowerCase();
+
+  for (let i = 0; i < formattedStr.length; i++) {
+    const char = formattedStr[i];
+
+    if (
+      char === "a" ||
+      char === "e" ||
+      char === "i" ||
+      char === "o" ||
+      char === "u"
+    ) {
+      count++;
+    }
+  }
+
+  console.log(count);
+
+  return count;
+}
+
+countVowels("mississippiou");
 
 module.exports = countVowels;
