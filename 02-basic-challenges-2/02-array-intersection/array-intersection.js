138036727058e95c0eee3679c5db6e335ee04725
AshwiniParaye1 <ashwiniparaye9309@gmail.com>
2024-11-25 11:16:23 +0530

    array intersection

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,14 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  const intersection = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2.includes(arr1[i]) && !intersection.includes(arr1[i])) {
+      intersection.push(arr1[i]);
+    }
+  }
+
+  return intersection;
+}
+arrayIntersection([1, 2, 3, 4, 5], [1, 3, 5, 7, 9]);
 
 module.exports = arrayIntersection;
